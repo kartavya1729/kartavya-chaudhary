8083c3223f72ab3e21076079aae6e23ab997e331
Kartavya Tomar <kartavyachaudharyofficial@gmail.com>
2024-03-21 15:04:20 +0530

    Update app.py

--- Digital-clock-in-Python-master/app.py
+++ Digital-clock-in-Python-master/app.py
@@ -6,10 +6,10 @@ window = Tk()
 window.title("")
 window.geometry("1000x800")
 window.configure(bg="green")  # =======Background of the clock=====
-window.resizable(False, False)  # =====setting a fixed window size =======
+window.resizable(True, True)  # =====setting a resizable window size =======
 
 clock_label = Label(
-    window, bg="black", fg="cyan", font=("Arial", 30, "bold"), relief="flat"
+    window, bg="black", fg="cyan", font=("Arial", 30, "bold"), relief="flat", padx = "20px", pady = "20px"
 )
 clock_label.place(x=500, y=800)
 
@@ -23,10 +23,8 @@ def update_label():
     current_time = strftime("%H: %M: %S\n %d-%m-%Y ")
     clock_label.configure(text=current_time)
     clock_label.after(80, update_label)
-    clock_label.pack(anchor="center")
+    clock_label.pack(anchor="center", pady = "250px")
 
 
 update_label()
 window.mainloop()
-
-# ==============The end by github.com/kalebu ==========
